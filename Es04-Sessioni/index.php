b06e17db474497a792e028fea8ce4fa0159a08ec
Wannaaaaaaa <113595533+Wannaaaaaaa@users.noreply.github.com>
2023-03-11 20:38:07 +0100

    Create index.php

---
+++ Es04-Sessioni/index.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<title>BENVENUTO</title>
+</head>
+<body>
+<h1>WEBAPP</h1>
+<a href="riservata.php">
+Vai all'area riservata del sito
+</a> 
+<br>
+<a href="login.php">
+Login
+</a> 
+</body>
+</html>
