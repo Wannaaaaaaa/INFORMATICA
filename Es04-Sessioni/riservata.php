228e308d209f7bdfdc8e574961ea52afd18e8613
Wannaaaaaaa <113595533+Wannaaaaaaa@users.noreply.github.com>
2023-03-11 20:39:46 +0100

    Create riservata.php

---
+++ Es04-Sessioni/riservata.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+?>
+
+<html>
+<head>
+<title>Riservata</title>
+</head>
+<body>
+<?php
+if(isset($_SESSION["username"])) 
+{
+?>
+    <p><h1>Benvenuto <?php echo $_SESSION["username"]; ?></h1></p>
+    <a href="index.php">Vai alla home del sito</a><br>
+    <a href="logout.php">Logout</a>
+    <?php   
+} 
+else 
+{
+    ?>
+    <p><h2>Non sei autenticato!</h2></p><br>
+    <a href="index.php">Vai alla home del sito</a><br>
+    <a href="login.php">Login</a>
+    <?php
+}
+?>
+</body>
+</html>
