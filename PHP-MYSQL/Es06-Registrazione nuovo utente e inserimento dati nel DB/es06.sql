2ed33b7442a80d5290de90cf9b32c1046008aa7d
Wannaaaaaaa <113595533+Wannaaaaaaa@users.noreply.github.com>
2023-03-12 21:00:53 +0100

    Create es06.sql

---
+++ PHP-MYSQL/Es06-Registrazione nuovo utente e inserimento dati nel DB/es06.sql
@@ -0,0 +1,35 @@
+//aggiunti nuovi attributi
+ALTER TABLE utente ADD COLUMN
+(
+    mail CHAR(20),
+    nascita DATE
+);
+
+//modifica dati istanza id 1
+UPDATE utente
+SET mail='flaviociao@gmail.com'
+WHERE id='1'
+
+//modifica dati istanza id 1
+UPDATE utente
+SET nascita='2004-01-27' 
+WHERE id='1';
+
+//inserimento dati per nuova istanza
+INSERT 
+INTO utente(id, username, pasword,mail,nascita)
+VALUES (NULL, 'Andrea', 'prova','andreaprova@gmail.com','2000-09-12');
+
+//aggiunti nuovi attributi
+ALTER TABLE utente ADD COLUMN(
+    nome CHAR(20),
+    cognome CHAR(20)
+);
+
+UPDATE utente
+SET nome='Flavio'
+WHERE id='1'
+
+UPDATE utente
+SET cognome='Wannaku'
+WHERE id='1'
