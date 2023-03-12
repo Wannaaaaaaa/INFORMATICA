f561a90931a074965b75d258f0ab913c353d488c
Wannaaaaaaa <113595533+Wannaaaaaaa@users.noreply.github.com>
2023-03-12 21:35:43 +0100

    Create riservata.php

---
+++ PHP-MYSQL/Es07-Web app PHP per la gestione dell'account/riservata.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+?>
+
+<html>
+<head>
+<title>Riservata</title>
+</head>
+<body>
+<?php
+if(isset($_SESSION["login"])) 
+{
+?>
+    <p><h1>Benvenuto nella pagina riservata!</h1></p>
+    <a href="index.php">Vai alla home del sito!</a><br>
+    <a href="logout.php">Logout</a>
+    <?php   
+} 
+else 
+{
+    ?>
+    <p><h2>Non sei autenticato!</h2></p><br>
+    <a href="index.php">Vai alla home del sito!</a><br>
+    <a href="login.php">Login!</a>
+    <?php
+}
+?>
+</body>
+</html>
