24293f8ceec2afd8935517d4621d3509ea111f13
Wannaaaaaaa <113595533+Wannaaaaaaa@users.noreply.github.com>
2023-03-13 22:09:13 +0100

    Update riservata.php

--- PHP-MYSQL/Es07-Web app PHP per la gestione dell'account/riservata.php
+++ PHP-MYSQL/Es07-Web app PHP per la gestione dell'account/riservata.php
@@ -11,7 +11,7 @@ session_start();
 if(isset($_SESSION["login"])) 
 {
 	?>
-	<p><h1>Benvenuto <?php echo $_SESSION["username"]; ?></h1></p>
+	<p><h1>Benvenuto <?php echo $_SESSION["login"]; ?></h1></p>
     <a href="index.php">Vai alla home del sito!</a><br>
     <a href="logout.php">Logout</a>
     <?php   
