d3893a51b690e8162f54a367792b74273327a9f9
Wannaaaaaaa <113595533+Wannaaaaaaa@users.noreply.github.com>
2023-03-16 11:20:41 +0100

    Update es05.sql

--- PHP-MYSQL/Es05-Login con PHP e MySql/es05.sql
+++ PHP-MYSQL/Es05-Login con PHP e MySql/es05.sql
@@ -6,11 +6,11 @@ CREATE TABLE utente
 (
     id INT NOT NULL AUTO_INCREMENT,
     username VARCHAR(20) NOT NULL,
-    pasword VARCHAR(20) NOT NULL,
+    password VARCHAR(20) NOT NULL,
     PRIMARY KEY (id)
 ) ENGINE=InnoDB AUTO_INCREMENT=1;
 
 //inserimento dati nuova istanza istanza 
 INSERT 
-INTO utente(id, username, pasword)
+INTO utente(id, username, password)
 VALUES (NULL, 'Flavio', 'ciao');
