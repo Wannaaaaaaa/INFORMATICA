8b8f19cb0d8c6f4795f5450e66ad80b8ede7d566
Wannaaaaaaa <113595533+Wannaaaaaaa@users.noreply.github.com>
2023-03-16 19:59:24 +0100

    Update funzioni.php

--- PHP-MYSQL/Es07-Web app PHP per la gestione dell'account/funzioni.php
+++ PHP-MYSQL/Es07-Web app PHP per la gestione dell'account/funzioni.php
@@ -79,9 +79,9 @@ function form_registrazione()
 	<input type="password" name="password"> 
 	<span class="errore">*</span>
     <p><input type="checkbox" required name="terms"> <a href="https://github.com/Wannaaaaaaa/INFORMATICA">Accetto i termini e le condizioni sull'informatica privacy</a></p>
-    <input type="submit"  name="invio" value="invio"> 
+    <input type="submit" name="invio" value="invio"> 
 	<br> <br>
-	<input type="reset"   value="cancella">
+	<input type="reset" value="cancella">
     </form>
 	<?php
 }
@@ -103,7 +103,7 @@ function registrazione()
 	
 	if($pdo->query($registr)==true)
 	{
-        echo "Registrazione avvenuta con successo!";
+        echo "Registrazione avvenuta con successo!<br>";
 		echo "<a href='login.php'>Login!</a><br>";
     }
 	else
@@ -114,16 +114,17 @@ function registrazione()
 	//termino connessione al database
 	$pdo=null;
 }
-function form_reset()
+function form_resetPass()
 {
 	?>
 	<font color=#FF0000>
     <h1>BENVENUTI NEL FORM DI RIPRISTINO PASSWORD</h1>
     </font>
     <h4>Inserire i dati richiesti:</h4>
-	<form name="formLogin" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
-	Nuova password: <input type="password" name="new"> <br> <br>
-	Conferma password: <input type="password" name="confirm"> </br> </br> 
+	<form name="formLogin" action="reset.php" method="POST">
+	Username: <input type="text" name="username"> <br> <br>
+	E-mail: <input type="text" name="email"> <br> <br>
+	Nuova password: <input type="password" name="password"> <br> <br>
 	<input type="submit"  name="invio" value="invio"> <br> <br>
 	<input type="reset"   value="cancella"> </br>
 	</form>
@@ -132,28 +133,76 @@ function form_reset()
 }
 function reset_password()
 {
-	$new=$_POST['new'];
-	$confirm=$_POST['confirm'];
-	$username=$_POST['username'];
-	$password=$_POST['password'];
+	//passo le variabili con POST
+	$username = $_POST['username'];
+        $password = $_POST['password'];
+	$mail = $_POST['email'];
 	
 	//query per database
 	$pdo = new PDO("mysql:host=".DB_SERVER.";dbname=".DB_NAME, DB_USER, DB_PASSWORD);
 	
-	//inserisco i dati
-	$reset = "UPDATE utente set pasword='$new','$confirm' WHERE username = '$username' AND password = '$password'";
-	
-	if($pdo->query($reset)==true)
+	//interogazione database
+	$interr = "SELECT * FROM utente WHERE username = '$username' AND mail = '$mail'";
+	//risultato interrogazione
+	$ris = $pdo->query($interr);
+	if($ris->rowCount()>0)
 	{
-        echo "Reset della password avvenuta con successo!";
-		echo "<a href='login.php'>Login!</a><br>";
-    }
+		//se vero
+		$_SESSION['login']=true;
+			
+		$pdo = new PDO("mysql:host=".DB_SERVER.";dbname=".DB_NAME, DB_USER, DB_PASSWORD);
+		$pasRes = "UPDATE utente SET password='$password' WHERE username='$username'";
+		//controllo se la query è andata a buon fine
+		if($pdo->query($pasRes)==true)
+		{
+			echo "Password aggiornata <br>";
+		}
+		else
+		    echo "Impossibile aggiornare la password <br>";
+	}
 	else
 	{
-        $_SESSION['login'] = false;
-		echo "Password non valida!";
+		//se falso
+		$_SESSION['login']=false;
+		echo "Errore! <a href='reset.php'> Cambia Password</a><br>";
 	}
-	//termino connessione al database
+	
+	//termino connessione al db
 	$pdo=null;
+}
+
+function cancUserForm()
+{
+	?>
+	<font color=#FF0000>
+    <h1>BENVENUTI NEL FORM DI RIPRISTINO PASSWORD</h1>
+    </font>
+	<form name="formLogin" action="deleteUser.php" method="POST">
+	E-mail: <input type="text" name="email"> <br> <br>
+	<input type="submit"  name="invio" value="invio"> <br> <br>
+	<input type="reset"   value="cancella"> </br>
+	</form>
+    <p><a href="index.php">home!</a>.</p>
+	<?php
+}
+
+function cancUser(){
+	$mail = $_POST['email'];
+
+    $pdo = new PDO("mysql:host=".DB_SERVER.";dbname=".DB_NAME, DB_USER, DB_PASSWORD);
+    $canc = "DELETE FROM utente WHERE mail='$mail'";
+		
+	//controllo se la query è andata a buon fine
+    if($pdo->query($canc)==true)
+	{
+        echo "Eliminazione avvenuta correttamente!<br>";
+    }
+	else
+		$_SESSION['login']=false;
+        echo "Impossibile eliminare l'account!<br>";
+	
+	//chiudo la connessione al database
+	$pdo=null;
+	session_destroy();
 }
 ?>
